6bdf6a2b98bbccc71df5308ae1c3760e8e988688
Chandan K P <chandanyuva1511@gmail.com>
2025-05-02 09:02:02 +0000

    ui updates

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ app.use("/messageDetails", messageDetailsRouter);
 const PORT = 3000;
 const HOST = "127.0.0.1";
 
-
 app.listen(PORT, HOST, () => {
     console.log(`Running Server on: http://${HOST}:${PORT}`);
 });

3054d24032184272ca07717d03954992bc2eb57e
Chandan K P <chandanyuva1511@gmail.com>
2025-05-02 06:13:26 +0000

    added new message and messagedetails

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
-const indexRouter = require("./routes/indexRouter.js");
+const indexRouter = require("./routers/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
 const PORT = 3000;
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -4,20 +4,42 @@ const indexRouter = Router();
 
 const messages = [
     {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
     },
     {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
-
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
 
 indexRouter.get("/", (req, res) => {
-    res.render("index", { title: "Mini Messageboard", messages: messages })
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+    // console.log(req.body.author);
+    // console.log(req.body.message);
+    const newMessage = {
+        user: req.body.author,
+        text: req.body.message,
+        added: new Date(),
+    };
+    messages.push(newMessage);
+    res.redirect("/");
+});
+
+indexRouter.get(`/messageDetails/:index`, (req, res) => {
+    const index = req.params.index;
+    const data = messages[index];
+    // console.log(data);
+    res.send(`date: ${data.added} | user: ${data.user} text: ${data.text}`);
 });
 
 module.exports = indexRouter;

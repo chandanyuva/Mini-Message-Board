817a05e817b5c50a932e69e0b415d6101893843d
Chandan K P <chandanyuva1511@gmail.com>
2025-05-02 04:48:37 +0000

    basic setup of - express - ejs - router/indexRouter - views/index.ejs - nodemon

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter.js");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Running Server on PORT:", PORT);
+});
